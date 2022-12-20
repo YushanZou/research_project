de00f1f797c07a2456b89f1604f055fa2febebca
YushanZou <113129008+YushanZou@users.noreply.github.com>
2022-12-20 15:01:44 -0500

    changed some code

---
+++ list2.js
@@ -0,0 +1,579 @@
+var list2=
+[
+    {
+      "sentence": "A balloon dropped and smashed a porcelain figure",
+      "pic": "figureS.jpg",
+      "correct": "S",
+      "condition": "Light_Contradictory",
+      "List": 2,
+      "object number": 1
+    },
+    {
+      "sentence": "A balloon dropped and smashed a piece of cake",
+      "pic": "cakeS.jpg",
+      "correct": "S",
+      "condition": "Light_Contradictory",
+      "List": 2,
+      "object number": 2
+    },
+    {
+      "sentence": "A balloon dropped and smashed a TV",
+      "pic": "tvS.jpg",
+      "correct": "S",
+      "condition": "Light_Contradictory",
+      "List": 2,
+      "object number": 3
+    },
+    {
+      "sentence": "A balloon dropped and smashed a cracker",
+      "pic": "crackerS.jpg",
+      "correct": "S",
+      "condition": "Light_Contradictory",
+      "List": 2,
+      "object number": 4
+    },
+    {
+      "sentence": "A balloon dropped and smashed a wineglass",
+      "pic": "wineglassS.jpg",
+      "correct": "S",
+      "condition": "Light_Contradictory",
+      "List": 2,
+      "object number": 5
+    },
+    {
+      "sentence": "A balloon dropped and smashed an egg",
+      "pic": "eggS.jpg",
+      "correct": "S",
+      "condition": "Light_Contradictory",
+      "List": 2,
+      "object number": 6
+    },
+    {
+      "sentence": "A bowling ball dropped and smashed an egg flan",
+      "pic": "eggflanS.jpg",
+      "correct": "S",
+      "condition": "Heavy_Matched",
+      "List": 2,
+      "object number": 7
+    },
+    {
+      "sentence": "A bowling ball dropped and smashed a blackberry",
+      "pic": "blackberryS.jpg",
+      "correct": "S",
+      "condition": "Heavy_Matched",
+      "List": 2,
+      "object number": 8
+    },
+    {
+      "sentence": "A bowling ball dropped and smashed glasses",
+      "pic": "glassesS.jpg",
+      "correct": "S",
+      "condition": "Heavy_Matched",
+      "List": 2,
+      "object number": 9
+    },
+    {
+      "sentence": "A bowling ball dropped and smashed a banana",
+      "pic": "bananaS.jpg",
+      "correct": "S",
+      "condition": "Heavy_Matched",
+      "List": 2,
+      "object number": 10
+    },
+    {
+      "sentence": "A bowling ball dropped and smashed a chocolate ice-cream",
+      "pic": "icecreamS.jpg",
+      "correct": "S",
+      "condition": "Heavy_Matched",
+      "List": 2,
+      "object number": 11
+    },
+    {
+      "sentence": "A bowling ball dropped and smashed an iPAD",
+      "pic": "iPadS.jpg",
+      "correct": "S",
+      "condition": "Heavy_Matched",
+      "List": 2,
+      "object number": 12
+    },
+    {
+      "sentence": "A bowling ball dropped and touched an iPhone",
+      "pic": "iphoneS.png",
+      "correct": "S",
+      "condition": "Heavy_Contradictory",
+      "List": 2,
+      "object number": 13
+    },
+    {
+      "sentence": "A bowling ball dropped and touched a light bulb",
+      "pic": "lampS.jpg",
+      "correct": "S",
+      "condition": "Heavy_Contradictory",
+      "List": 2,
+      "object number": 14
+    },
+    {
+      "sentence": "A bowling ball dropped and touched a mirror",
+      "pic": "mirrorS.jpg",
+      "correct": "S",
+      "condition": "Heavy_Contradictory",
+      "List": 2,
+      "object number": 15
+    },
+    {
+      "sentence": "A bowling ball dropped and touched a chocolate muffin",
+      "pic": "muffinS.jpg",
+      "correct": "S",
+      "condition": "Heavy_Contradictory",
+      "List": 2,
+      "object number": 16
+    },
+    {
+      "sentence": "A bowling ball dropped and touched a donut",
+      "pic": "donutS.jpg",
+      "correct": "S",
+      "condition": "Heavy_Contradictory",
+      "List": 2,
+      "object number": 17
+    },
+    {
+      "sentence": "A bowling ball dropped and touched a rice cake",
+      "pic": "tortitaS.jpg",
+      "correct": "S",
+      "condition": "Heavy_Contradictory",
+      "List": 2,
+      "object number": 18
+    },
+    {
+      "sentence": "A balloon dropped and touched a ceramic plate",
+      "pic": "plateS.jpg",
+      "correct": "S",
+      "condition": "Light_Matched",
+      "List": 2,
+      "object number": 19
+    },
+    {
+      "sentence": "A balloon dropped and touched a strawberry",
+      "pic": "strawberryS.jpg",
+      "correct": "S",
+      "condition": "Light_Matched",
+      "List": 2,
+      "object number": 20
+    },
+    {
+      "sentence": "A balloon dropped and touched a piece of sushi",
+      "pic": "sushiS.jpg",
+      "correct": "S",
+      "condition": "Light_Matched",
+      "List": 2,
+      "object number": 21
+    },
+    {
+      "sentence": "A balloon dropped and touched a tile",
+      "pic": "tileS.jpg",
+      "correct": "S",
+      "condition": "Light_Matched",
+      "List": 2,
+      "object number": 22
+    },
+    {
+      "sentence": "A balloon dropped and touched a tomato",
+      "pic": "tomatoS.jpg",
+      "correct": "S",
+      "condition": "Light_Matched",
+      "List": 2,
+      "object number": 23
+    },
+    {
+      "sentence": "A balloon dropped and touched a vase",
+      "pic": "vaseS.jpg",
+      "correct": "S",
+      "condition": "Light_Matched",
+      "List": 2,
+      "object number": 24
+    },
+    {
+      "sentence": "A toothbrush was placed on the sponge",
+      "pic": "sponge.jpg",
+      "correct": "S",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A fork was placed on the plate",
+      "pic": "plate.jpg",
+      "correct": "S",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A musician is playing a guitar",
+      "pic": "guitar.jpg",
+      "correct": "S",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A cat is drinking water from a bowl",
+      "pic": "bowl.jpg",
+      "correct": "S",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "An old man is hammering a nail",
+      "pic": "nail.jpg",
+      "correct": "S",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A boy is wearing a hat",
+      "pic": "hat.jpg",
+      "correct": "S",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "An elephant is eating a watermelon",
+      "pic": "watermelon.jpg",
+      "correct": "S",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A postman is driving a car",
+      "pic": "car.jpg",
+      "correct": "S",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "An astronaut is putting on a space suit",
+      "pic": "spacesuit.jpg",
+      "correct": "S",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A cook is taking a knife",
+      "pic": "knife.jpg",
+      "correct": "S",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A photographer is taking a photo of a lion",
+      "pic": "lion.jpg",
+      "correct": "S",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A puppy is playing with a ball",
+      "pic": "tennisball.jpg",
+      "correct": "S",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A cabbage was placed on the table",
+      "pic": "ladder.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A keyboard was placed on the bed",
+      "pic": "bottlecap.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A watch was placed on the laptop",
+      "pic": "bow.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A cushion was placed on the magazine",
+      "pic": "box.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A remote was placed on the rug",
+      "pic": "CD.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A pencil was placed on the shoe",
+      "pic": "perfume.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A thermometer was placed on the box",
+      "pic": "button.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A ring was placed on the newspaper",
+      "pic": "cat.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A needle was placed on the mouse pad",
+      "pic": "chocolate.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A man is shaving his beard",
+      "pic": "clock.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A woman is taking money out of a wallet",
+      "pic": "coconut.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A donkey is drinking water.",
+      "pic": "couch.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A man is boiling potatoes.",
+      "pic": "cup.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A dentist is drinking a can of cola",
+      "pic": "doll.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A nurse is taking a syringe",
+      "pic": "headphones.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A doctor is taking a stethoscope",
+      "pic": "matchesbox.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A student is writing on a board",
+      "pic": "owl.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "An actress is taking a microphone",
+      "pic": "pants.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A kitten is playing in the sandbox",
+      "pic": "dustbin.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A towel was placed on the TV",
+      "pic": "pencil.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A teenage girl is playing the piano",
+      "pic": "pillow.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A horse is chewing hay",
+      "pic": "purse.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A mouse is eating cheese",
+      "pic": "ring.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A panda is resting on a tree",
+      "pic": "dollar.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "An owl is sitting on the branch",
+      "pic": "rubberduck.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A parrot is eating an apple",
+      "pic": "shirt.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A butterfly is sitting on a flower",
+      "pic": "soap.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A hairdresser is taking scissors",
+      "pic": "sock.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A pilot is reading a book",
+      "pic": "spoon.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A thief is stealing a wallet",
+      "pic": "teddybear.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A police officer is talking on the phone",
+      "pic": "tissuebox.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A president is writing in the notebook",
+      "pic": "can.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A woman is paying with a debit card",
+      "pic": "papercup.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A giraffe is playing with a zebra",
+      "pic": "wallet.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A zebra is playing with a horse",
+      "pic": "watch.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    },
+    {
+      "sentence": "A man is playing with a cat",
+      "pic": "ceramicplate.jpg",
+      "correct": "N",
+      "condition": "Filler",
+      "List": 2,
+      "object number": ""
+    }
+  ]
---
+++ practice.js
@@ -0,0 +1,50 @@
+var practicestimuli=[
+    {
+      "index": 99,
+      "sentence": "A bowling ball dropped and smashed a teacup",
+      "pic": "cupB.jpg",
+      "correct": "S",
+      "condition": "practice_s",
+      "List": "practice"
+    },
+    {
+      "index": 99,
+      "sentence": "A balloon dropped and touched a cigarette",
+      "pic": "cigaretteB.jpg",
+      "correct": "S",
+      "condition": "practice_s",
+      "List": "practice"
+    },
+    {
+      "index": 99,
+      "sentence": "A knife was placed on a hammer",
+      "pic": "hammerB.jpg",
+      "correct": "S",
+      "condition": "practice_s",
+      "List": "practice"
+    },
+    {
+      "index": 99,
+      "sentence": "A bowling ball dropped and touched a bottle",
+      "pic": "bookB.jpg",
+      "correct": "N",
+      "condition": "practice_n",
+      "List": "practice"
+    },
+    {
+      "index": 99,
+      "sentence": "A balloon dropped and smashed a cup",
+      "pic": "bananaB.jpg",
+      "correct": "N",
+      "condition": "practice_n",
+      "List": "practice"
+    },
+    {
+      "index": 99,
+      "sentence": "A fork was placed on a stool",
+      "pic": "tableB.jpg",
+      "correct": "N",
+      "condition": "practice_n",
+      "List": "practice"
+    }
+  ]
